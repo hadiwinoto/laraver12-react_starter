31d193aa062a419f7031527a8a897bb6d15c423b
Hadi winoto <hadiwinoto@Hadis-Macbook-Pro.local>
2025-02-28 15:33:25 +0700

    update

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -11,7 +11,7 @@ import { useInitials } from '@/hooks/use-initials';
 import { cn } from '@/lib/utils';
 import { type BreadcrumbItem, type NavItem, type SharedData } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid, Menu, Search } from 'lucide-react';
+import { Folder, LayoutGrid, Menu } from 'lucide-react';
 import AppLogo from './app-logo';
 import AppLogoIcon from './app-logo-icon';
 
@@ -29,11 +29,11 @@ const rightNavItems: NavItem[] = [
     //     url: '#',
     //     icon: Folder,
     // },
-    // {
-    //     title: 'Documentation',
-    //     url: '#',
-    //     icon: BookOpen,
-    // },
+    {
+        title: 'Documents',
+        url: '#',
+        icon: Folder,
+    },
 ];
 
 const activeItemStyles = 'text-neutral-900 dark:bg-neutral-800 dark:text-neutral-100';
