6f28446029e3f759c2c364fd2087facba602522e
Hadi winoto <hadiwinoto@Hadis-Macbook-Pro.local>
2025-02-28 11:02:55 +0700

    update

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -169,7 +169,7 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
             </div>
             {breadcrumbs.length > 0 && (
                 <div className="border-sidebar-border/70 flex w-full border-b">
-                    <div className="mx-auto flex h-10 w-full items-center justify-start px-4 text-neutral-500 md:max-w-7xl">
+                    <div className="mx-auto flex h-9 w-full items-center justify-start px-4 text-neutral-500 md:max-w-7xl">
                         <Breadcrumbs breadcrumbs={breadcrumbs} />
                     </div>
                 </div>
